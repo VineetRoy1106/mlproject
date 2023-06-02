09c93852b91e57022013b2f82e85545941ce9b7f
v-roy <barcamessi1106@example.com>
2023-06-03 02:14:53 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -106,13 +106,12 @@ class DataTransformation:
             ]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
-            logging.info(f"Saved preprocessing object.")
+            logging.info("Saved preprocessing object.")
 
             save_object(
 
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
-
             )
 
             return (
